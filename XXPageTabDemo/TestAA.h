25a5f54e8cdaebba34622ed30dee81f80923f057
Jie Huang <517050790@qq.com>
2018-09-27 14:57:45 +0800

    新增文件

---
+++ XXPageTabDemo/TestAA.h
@@ -0,0 +1,13 @@
+//
+//  TestAA.h
+//  XXPageTabDemo
+//
+//  Created by Huang,Jie(Int'l RD) on 2018/9/27.
+//  Copyright © 2018年 HJTXX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestAA : NSObject
+
+@end
---
+++ XXPageTabDemo/TestAA.m
@@ -0,0 +1,13 @@
+//
+//  TestAA.m
+//  XXPageTabDemo
+//
+//  Created by Huang,Jie(Int'l RD) on 2018/9/27.
+//  Copyright © 2018年 HJTXX. All rights reserved.
+//
+
+#import "TestAA.h"
+
+@implementation TestAA
+
+@end
